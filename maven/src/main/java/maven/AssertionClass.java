2269b0c25384dede58679e6428a5585174672f80
akshayjain <akshayjain@Akshay>
2019-05-20 03:25:33 +0530

    test commit2

---
+++ maven/src/main/java/maven/AssertionClass.java
@@ -0,0 +1,45 @@
+package maven;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AssertionClass {
+	@Test
+	public void test()
+	{
+		Assert.assertTrue(true);
+	
+	}
+	@Test
+	public void test1()
+	{
+		Assert.assertTrue(false);
+	}
+	@Test
+	public void test2()
+	{
+		Assert.assertEquals("Test","Test");
+		
+	}
+	@Test
+	public void test3()
+	{
+		Assert.assertEquals("Test","Test2");
+	}
+	@Test
+	public void test4()
+	{
+		Assert.assertTrue(false,"not matching the expected condition");
+	}
+	@Test
+	public void test5()
+	{
+		Assert.assertFalse(false,"This is expected");
+	}
+	@Test
+	public void test6()
+	{
+		Assert.assertNotEquals("test","test1");
+	}
+
+}
